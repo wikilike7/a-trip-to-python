a1a77c0344776e5ea0779bae34ed26fc75dce3a8
wikilike7 <wikilike7@gmail.com>
2017-12-06 17:33:16 +0800

    update 函数.py

--- 函数.py
+++ 函数.py
@@ -41,5 +41,47 @@ def right_justfy(s):
     print(' ' * (70 - len(s)), end=s)
 
 
-right_justfy('zibba11')  # test pass
+right_justfy('zibba')  # test pass
 
+
+print('\n')  # print a blank line
+
+
+# # Exercise 2
+# def print_twice(arg):
+#     print(arg)
+#     print(arg)
+#
+#
+# def do_twice(func, arg):
+#     func(arg)
+#     func(arg)
+#
+#
+# def do_four(func, arg):
+#     do_twice(func, arg)
+#     do_twice(func, arg)
+#
+#
+# do_twice(print_twice, 'spam')
+# print('')
+# do_four(do_twice, 'spam')
+
+
+# Exercise 3
+print('+', '-' * 4, '+', '-' * 4, '+')
+print('|', ' ' * 4, '|', ' ' * 4, '|')
+print('|', ' ' * 4, '|', ' ' * 4, '|')
+print('|', ' ' * 4, '|', ' ' * 4, '|')
+print('|', ' ' * 4, '|', ' ' * 4, '|')
+
+
+def print_table(num):
+    print('+', '-' * num, '+', '-' * num, '+')
+    print('|', ' ' * num, '|', ' ' * num, '|')
+    print('+', '-' * num, '+', '-' * num, '+')
+    print('|', ' ' * num, '|', ' ' * num, '|')
+    print('+', '-' * num, '+', '-' * num, '+')
+
+
+print_table('h')
